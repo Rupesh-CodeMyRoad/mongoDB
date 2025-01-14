25e230fadab81a862cc039279cc750405772c1d2
Rupesh Gaudel Regmi <rupeshgaudel3@gmail.com>
2025-01-14 18:43:27 +0400

    New Project MongoDb

---
+++ src/test/java/com/rupesh/joblisting/MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupesh.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
